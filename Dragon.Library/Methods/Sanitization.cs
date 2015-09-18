b4634ff204c8bd9bb93454bedb2e0c4b0765a5e7
Steven <hsgaming@gmail.com>
2015-09-17 18:05:35 -0700

    Sanitization
    Making Sure usernames, passwords and emails are valid.

--- Dragon.Library/Methods/Sanitization.cs
+++ Dragon.Library/Methods/Sanitization.cs
@@ -7,7 +7,24 @@ using System.Text.RegularExpressions;
 
 namespace Dragon.Library.Methods
 {
-    class Sanitization
+   public static class Sanitization
     {
+       public static bool cleanEmail(string s)
+        {   //Could also use the Mailing Class system
+            return Regex.IsMatch(s, @"^(?("")("".+?(?<!\\)""@)|(([0-9a-z]((\.(?!\.))|[-!#\$%&'\*\+/=\?\^`\{\}\|~\w])*)(?<=[0-9a-z])@))" +
+                @"(?(\[)(\[(\d{1,3}\.){3}\d{1,3}\])|(([0-9a-z][-\w]*[0-9a-z]*\.)+[a-z0-9][\-a-z0-9]{0,22}[a-z0-9]))$",
+                RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
+        }
+
+       public static bool cleanPassword(string s)
+        {   //Check for UTF-8, But all strings in .NET should be UTF-16
+            return Regex.IsMatch(s, @"^(\w[6])+{\w[16]$", RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
+        }
+
+       public static bool cleanUsername(string s)
+       {
+           return Regex.IsMatch(s, @"^(\w[24])$", RegexOptions.IgnoreCase, TimeSpan.FromMilliseconds(250));
+       }
+
     }
 }
